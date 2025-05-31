ef7f41690908da3a48d97b9d94705aa179cbc686
Steve <sbrownpilot@gmail.com>
2025-05-31 08:46:14 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unusualwhales',
+    version='0.3.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='SDK wrapper for Unusual Whales option flow API',
+    author='sbrownpilot',
+    author_email='none@example.com',
+    url='https://github.com/sbrownpilot/unusualwhales-sdk',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
